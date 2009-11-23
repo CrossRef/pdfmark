7d8fc0ab43ee87132c8a551d18fce2b90b7116ed
Karl Jonathan Ward <karl.j.ward@googlemail.com>
2009-11-23 16:09:05 +0000

    Main.java: Finished playing around with XMP extract/insert.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -11,32 +12,51 @@ import com.lowagie.text.pdf.PdfStamper;
 public class Main {
 
 	public static void main(String[] args) {
-		try {
-			// Grab the XMP
-			DataInputStream din = new DataInputStream(new FileInputStream(new File(args[0])));
+		if (args[0].equals("-i")) {
 		
-			byte[] buff = new byte[1024], xmpData = new byte[0];
-			int read = 0;
-			while ((read = din.read(buff, 0, buff.length)) > 0) {
-				byte[] tmp = new byte[xmpData.length + read];
-				System.arraycopy(xmpData, 0, tmp, 0, xmpData.length);
-				System.arraycopy(buff, 0, tmp, xmpData.length, buff.length);
+			try {
+				// Grab the XMP
+				DataInputStream din = new DataInputStream(new FileInputStream(new File(args[1])));
+			
+				byte[] buff = new byte[1024], xmpData = new byte[0];
+				int read = 0;
+				while ((read = din.read(buff, 0, buff.length)) > 0) {
+					byte[] tmp = new byte[xmpData.length + read];
+					System.arraycopy(xmpData, 0, tmp, 0, xmpData.length);
+					System.arraycopy(buff, 0, tmp, xmpData.length, read);
+				}
+				
+				din.close();
+				
+				// Insert the XMP into the document as XMP data.
+				PdfReader reader = new PdfReader(new FileInputStream(new File(args[2])));
+				PdfStamper stamper = new PdfStamper(reader, new FileOutputStream(new File(args[3])));
+				
+				stamper.setXmpMetadata(xmpData);
+				
+				stamper.close();
+				reader.close();
+			} catch (IOException e ) {
+				System.err.println(e);
+			} catch (DocumentException e) {
+				System.err.println(e);
 			}
 			
-			din.close();
+		} else if (args[0].equals("-x")) {
 			
-			// Insert the XMP into the document as XMP data.
-			PdfReader reader = new PdfReader(new FileInputStream(new File(args[1])));
-			PdfStamper stamper = new PdfStamper(reader, new FileOutputStream(new File(args[2])));
+			try {
+				// Grab XMP from a reader and dump it.
+				PdfReader reader = new PdfReader(new FileInputStream(new File(args[1])));
+				byte[] metadata = reader.getMetadata();
+				reader.close();
+				
+				DataOutputStream dout = new DataOutputStream(new FileOutputStream(new File(args[2])));
+				dout.write(metadata);
+				dout.close();
+			} catch (IOException e) {
+				System.err.println(e);
+			}
 			
-			stamper.setXmpMetadata(xmpData);
-			
-			stamper.close();
-			reader.close();
-		} catch (IOException e ) {
-			System.err.println(e);
-		} catch (DocumentException e) {
-			System.err.println(e);
 		}
 	}
 
